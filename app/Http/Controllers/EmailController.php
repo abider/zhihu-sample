3a24bde4c4c57d9d4ee446f8e57cc2db4de3bca8
vagrant <vagrant@homestead>
2017-02-02 11:20:12 +0000

    make repository by User model

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+use App\Repositories\Users;
 
 class EmailController extends Controller
 {
+    protected $user;
+
+    public function __construct(Users $user)
+    {
+        $this->user = $user;
+    }
+
     public function confirm($token)
     {
-        $user = User::where('confirmation_token', $token)->first();
-        $user->confirmation_token = str_random(60);
-        $user->is_active = 1;
-        $user->save();
+        $user = $this->user->findWhere(['confirmation_token' => $token]);
+        $this->user->update([
+            'confirmation_token' => str_random(60),
+            'is_active' => 1
+        ], $user->id);
         auth()->login($user);
 
         return redirect()->home();
---
+++ app/Repositories/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+
+class Users extends BaseRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
+}
