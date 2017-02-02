f06e9a079a8974227d88c4710944d8126fc2357b
vagrant <vagrant@homestead>
2017-02-02 09:14:01 +0000

    update users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name')->unique(); // 用户名
+            $table->string('email')->unique(); // 邮箱
+            $table->string('password'); // 密码
+            $table->string('avatar')->default('/img/avatars/default.png'); // 头像
+            $table->string('confirmation_token'); // 邮箱验证token
+            $table->smallInteger('is_active')->default(0); // 邮箱验证状态
+            $table->integer('questions_count')->default(0); // 问题量
+            $table->integer('answers_count')->default(0); // 回答量
+            $table->integer('comments_count')->default(0); // 评论量
+            $table->integer('favourites_count')->default(0);  // 收藏量
+            $table->integer('likes_count')->default(0); // 点赞量
+            $table->integer('followers_count')->default(0); // 关注量
+            $table->integer('followings_count')->default(0); // 被关注量
+            $table->json('setting')->nullable(); // 个人信息和设置
             $table->rememberToken();
             $table->timestamps();
         });
